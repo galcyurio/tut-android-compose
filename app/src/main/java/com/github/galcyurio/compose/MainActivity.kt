3ab09c78f6f497041a46c0aa7a96eb00d9a4ace2
galcyurio <galcyurio@gmail.com>
2022-08-19 01:54:10 +0900

    step1

--- app/src/main/java/com/github/galcyurio/compose/MainActivity.kt
+++ app/src/main/java/com/github/galcyurio/compose/MainActivity.kt
@@ -16,28 +16,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TutAndroidComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(name = "Hello, world!")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(name: String) {
+    Text(text = name)
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    TutAndroidComposeTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(name = "Android")
 }
