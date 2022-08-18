8cb07d165132d018c727f07c619b12772021dbdf
galcyurio <galcyurio@gmail.com>
2022-08-19 02:04:22 +0900

    step2

--- app/src/main/java/com/github/galcyurio/compose/MainActivity.kt
+++ app/src/main/java/com/github/galcyurio/compose/MainActivity.kt
@@ -3,31 +3,65 @@ package com.github.galcyurio.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.galcyurio.compose.ui.theme.TutAndroidComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "Hello, world!")
+            MessageCard(
+                message = Message(
+                    author = "galcyurio",
+                    body = "Hello, world!"
+                )
+            )
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = name)
+fun MessageCard(message: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = message.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = message.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "Android")
+    MessageCard(
+        message = Message(
+            author = "Colleague",
+            body = "Hey, take a look at Jetpack Compose, it's great!"
+        )
+    )
 }
