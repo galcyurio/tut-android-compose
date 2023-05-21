85eacedb4668859473a0652f5f2e853e3ffed30b
galcyurio <galcyurio@gmail.com>
2023-05-21 22:12:38 +0900

    step3

--- app/src/main/java/com/github/galcyurio/compose/MainActivity.kt
+++ app/src/main/java/com/github/galcyurio/compose/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.github.galcyurio.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,17 +24,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.github.galcyurio.compose.ui.theme.TutAndroidComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(
-                message = Message(
-                    author = "galcyurio",
-                    body = "Hello, world!"
-                )
-            )
+            TutAndroidComposeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("galcyurio", "Hello, world!"))
+                }
+            }
         }
     }
 }
@@ -45,23 +50,46 @@ fun MessageCard(message: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = message.author)
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2,
+            )
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = message.body)
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+            ) {
+                Text(
+                    text = message.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 4.dp),
+                )
+            }
         }
     }
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        message = Message(
-            author = "Colleague",
-            body = "Hey, take a look at Jetpack Compose, it's great!"
-        )
-    )
+    TutAndroidComposeTheme {
+        Surface {
+            MessageCard(
+                message = Message(
+                    author = "Colleague",
+                    body = "Hey, take a look at Jetpack Compose, it's great!"
+                )
+            )
+        }
+    }
 }
